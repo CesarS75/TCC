167b09a6f29862f6ef951ec675f03dfca9a8d3ee
aninhasentafofo <106964549+poisonrin@users.noreply.github.com>
2024-08-26 09:32:17 -0300

    comiiit

--- tccdiario-naomexe/js/mode.js
+++ tccdiario-naomexe/js/mode.js
@@ -8,19 +8,24 @@ localStorage.getItem('theme');
 const toggle = document.getElementById("toggle");
 const refresh = document.getElementById("refresh");
 const theme = window.localStorage.getItem("theme");
+const imagem = document.querySelector(".imgbotao");
 
 /* verifica se o tema armazenado no localStorage é escuro
 se sim aplica o tema escuro ao body */
-if (theme === "dark") document.body.classList.add("dark");
+if (theme === "dark") {
+  document.body.classList.add("dark");
+  imagem.setAttribute("src","./images/sun.png");
+}
 
 // event listener para quando o botão de alterar o tema for clicado
 toggle.addEventListener("click", () => {
+
   document.body.classList.toggle("dark");
   if (theme === "dark") {
     window.localStorage.setItem("theme", "light");
-  } else window.localStorage.setItem("theme", "dark");
-});
-
-refresh.addEventListener("click", () => {
-  window.location.reload();
+    imagem.setAttribute("src","./images/sun.png");
+  }
+  else
+    window.localStorage.setItem("theme", "dark");
+    imagem.setAttribute("src","./images/moon.png");
 });
