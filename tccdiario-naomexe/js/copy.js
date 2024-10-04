88ae96fb0282f34b33cf6294a14f2e6a488d997b
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2024-10-04 14:24:06 -0300

    modal e copy
    dois botao de copiar distintos pra cada página funfando ok

---
+++ tccdiario-naomexe/js/copy.js
@@ -0,0 +1,74 @@
+/* Função pra copiar texto HTML (1 a 6) */
+function copiarTextoHtml(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+function copiarTextoHtml2(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+function copiarTextoHtml3(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+function copiarTextoHtml4(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+function copiarTextoHtml5(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+function copiarTextoHtml6(){
+    var conteudo = document.getElementById('codigoHtml').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:"+ conteudo)
+}
+/* agora é função pra copiar texto CSS (1 a 6) */
+function copiarTextoCss(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
+function copiarTextoCss2(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
+function copiarTextoCss3(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
+function copiarTextoCss4(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
+function copiarTextoCss5(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
+function copiarTextoCss6(){
+    var conteudo = document.getElementById('codigoCss2').innerHTML;
+
+    navigator.clipboard.writeText(conteudo);
+    console.log("o texto copiado foi:" + conteudo)
+}
